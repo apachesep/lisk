b9a0709280948f75c2d08a54ba38fe52472f947a
Oliver Beddows <oliver@lightcurve.io>
2017-11-23 22:23:26 +0100

    Revise comments

--- sql/migrations/20171022144042_migrate_second_signature.sql
+++ sql/migrations/20171022144042_migrate_second_signature.sql
@@ -6,12 +6,12 @@ BEGIN
 END
 $$;
 
-/* Begin second signature migration */
 CREATE TABLE "public".second_signature(transaction_id varchar(20) NOT NULL,
   public_key bytea NOT NULL,
   second_public_key bytea NOT NULL,
   CONSTRAINT pk_second_signature PRIMARY KEY(public_key));
 
+-- Begin second signature migration
 
 INSERT INTO second_signature(transaction_id, public_key, second_public_key)
 SELECT t.transaction_id,
@@ -24,12 +24,12 @@ AND t."sender_address" = ma."address"
 AND t.type = 1;
 
 
-  -- Second signature indexes
   CREATE INDEX idx_second_signature_transaction_id
   ON "public".second_signature ( "transaction_id" );
 
   CREATE INDEX idx_public_key
   ON "public".second_signature ( "public_key" );
+-- Create second signature indexes
 
   CREATE INDEX idx_second_public_key
   ON "public".second_signature ( "second_public_key" );
