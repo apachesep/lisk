2b43cb7db6b9024d89a657b9fe1f4471e7b1541a
Oliver Beddows <oliver@lightcurve.io>
2017-11-23 15:10:36 +0100

    Indent using tabs

--- sql/migrations/20171022144040_migrate_accounts.sql
+++ sql/migrations/20171022144040_migrate_accounts.sql
@@ -9,25 +9,25 @@ $$;
 -- Apply transactions into new table, to populate new accounts table
 INSERT INTO "public".accounts(address, public_key, balance)
 SELECT
-  mem.address,
-  mem."publicKey",
-  mem.balance
+	mem.address,
+	mem."publicKey",
+	mem.balance
 FROM mem_accounts mem;
 
 -- UPDATE all accounts with first transaction
 UPDATE "public".accounts AS a set transaction_id = t.transaction_id
 FROM(
-  SELECT t."transaction_id", t."recipient_address"
-  FROM transactions AS t group by t."recipient_address", t."transaction_id"
+	SELECT t."transaction_id", t."recipient_address"
+	FROM transactions AS t group by t."recipient_address", t."transaction_id"
 ) AS t
 WHERE UPPER(t."recipient_address") = a."address";
 
 -- UPDATE genesis transaction ID
 UPDATE "public".accounts AS a set transaction_id = t.transaction_id
 FROM(
-  SELECT t."transaction_id", t."sender_address"
-  FROM transactions AS t WHERE t.type = '2'
-  group by t."recipient_address", t."transaction_id"
+	SELECT t."transaction_id", t."sender_address"
+	FROM transactions AS t WHERE t.type = '2'
+	GROUP BY t."recipient_address", t."transaction_id"
 ) AS t
 WHERE UPPER(t."sender_address") = a."address"
 AND a.transaction_id IS NULL;
@@ -35,16 +35,16 @@ AND a.transaction_id IS NULL;
 -- UPDATE all accounts with public keys
 UPDATE "public".accounts AS a set public_key_transaction_id = t.transaction_id
 FROM(
-  SELECT t."transaction_id", t."sender_public_key"
-  FROM transactions AS t group by t."sender_public_key", t."transaction_id"
+	SELECT t."transaction_id", t."sender_public_key"
+	FROM transactions AS t group by t."sender_public_key", t."transaction_id"
 ) AS t
 WHERE t."sender_public_key" = a."public_key";
 
 -- UPDATE all accounts with transaction IDs
 UPDATE "public".accounts AS a set transaction_id = t.transaction_id
 FROM(
-  SELECT t."transaction_id", t."recipient_address"
-  FROM transactions AS t
+	SELECT t."transaction_id", t."recipient_address"
+	FROM transactions AS t
 	group by t."recipient_address", t."transaction_id"
 ) AS t
 WHERE t."recipient_address" = a."address";
