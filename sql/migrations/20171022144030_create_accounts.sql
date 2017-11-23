1d8712c5575cdf7570c9657c552eb848732176d8
Oliver Beddows <oliver@lightcurve.io>
2017-11-23 20:33:46 +0100

    Standardise code

--- sql/migrations/20171022144030_create_accounts.sql
+++ sql/migrations/20171022144030_create_accounts.sql
@@ -29,7 +29,7 @@ CREATE OR REPLACE FUNCTION protect_accounts_balance() RETURNS TRIGGER LANGUAGE P
 
 		-- Skip check if there is no genesis block in database (in case of genesis block deletion)
 		IF genesis_block_id IS NULL THEN
-		    RETURN NEW;
+			RETURN NEW;
 		END IF;
 
 		-- Return TRUE if address belongs to sender of type 0 transaction that was included in genesis block
@@ -49,55 +49,68 @@ CREATE TRIGGER protect_accounts_balance
 
 CREATE OR REPLACE FUNCTION public.public_key_rollback() RETURNS TRIGGER LANGUAGE PLPGSQL AS $function$
 	BEGIN
-	RAISE NOTICE 'public_key_rollback';
+		RAISE NOTICE 'public_key_rollback';
 		NEW.public_key = NULL;
-		RETURN NEW;
-	END $function$ ;
+	RETURN NEW;
+END $function$ ;
 
-	CREATE TRIGGER public_key_rollback
+CREATE TRIGGER public_key_rollback
 	BEFORE UPDATE ON accounts
 	FOR EACH ROW WHEN (OLD.public_key_transaction_id IS NOT NULL AND NEW.public_key_transaction_id IS NULL)
 	EXECUTE PROCEDURE public_key_rollback();
 
 CREATE FUNCTION public.validate_accounts_balances() RETURNS TABLE(address VARCHAR(22), public_key TEXT, delegate VARCHAR(20), blockchain BIGINT, memory BIGINT, diff BIGINT) LANGUAGE PLPGSQL AS $$
-BEGIN
-	RETURN QUERY
-		WITH balances AS (
-			(SELECT UPPER(sender_address) AS address, -SUM(amount+fee) AS amount FROM transactions GROUP BY UPPER(sender_address))
-				UNION ALL
-			(SELECT UPPER(recipient_address) AS address, SUM(amount) AS amount FROM transactions WHERE recipient_address IS NOT NULL GROUP BY UPPER(recipient_address))
-				UNION ALL
-					(SELECT a.address, r.amount FROM
-						(SELECT r.public_key, SUM(r.fees) + SUM(r.reward) AS amount FROM rounds_rewards r GROUP BY r.public_key) r LEFT JOIN accounts a ON r.public_key = a.public_key
-					)
-		),
-		accounts_b AS (SELECT b.address, SUM(b.amount) AS balance FROM balances b GROUP BY b.address)
-		SELECT m.address, ENCODE(m.public_key, 'hex') AS public_key, d.name, a.balance::BIGINT AS blockchain, m.balance::BIGINT AS memory, (m.balance-a.balance)::BIGINT AS diff
-		FROM accounts a
-		LEFT JOIN accounts m ON a.address = m.address
-		LEFT JOIN delegates d ON a.address = d.address
-		WHERE a.balance <> m.balance;
+	BEGIN
+		RETURN QUERY
+			WITH balances AS (
+				(
+					SELECT UPPER(sender_address) AS address, -SUM(amount+fee) AS amount FROM transactions GROUP BY UPPER(sender_address)
+				)
+				UNION ALL (
+					SELECT UPPER(recipient_address) AS address, SUM(amount) AS amount FROM transactions WHERE recipient_address IS NOT NULL GROUP BY UPPER(recipient_address)
+				)
+				UNION ALL (
+					SELECT a.address, r.amount FROM (
+						SELECT r.public_key, SUM(r.fees) + SUM(r.reward) AS amount
+						FROM rounds_rewards r
+						GROUP BY r.public_key
+					) r
+					LEFT JOIN accounts a ON r.public_key = a.public_key
+				)
+			),
+			accounts_b AS (
+				SELECT b.address, SUM(b.amount) AS balance
+				FROM balances b
+				GROUP BY b.address
+			)
+			SELECT m.address, ENCODE(m.public_key, 'hex') AS public_key, d.name, a.balance::BIGINT AS blockchain, m.balance::BIGINT AS memory, (m.balance-a.balance)::BIGINT AS diff
+			FROM accounts a
+			LEFT JOIN accounts m ON a.address = m.address
+			LEFT JOIN delegates d ON a.address = d.address
+			WHERE a.balance <> m.balance;
 END $$;
 
-CREATE OR REPLACE FUNCTION public.revert_mem_account() RETURNS TRIGGER LANGUAGE PLPGSQL AS $function$ BEGIN IF NEW."address" <> OLD."address" THEN
-	RAISE WARNING 'Reverting change of address from % to %', OLD."address", NEW."address"; NEW."address" = OLD."address";
-	END IF; IF NEW."u_username" <> OLD."u_username"
-		AND OLD."u_username" IS NOT NULL THEN
-	RAISE WARNING 'Reverting change of u_username from % to %', OLD."u_username", NEW."u_username"; NEW."u_username" = OLD."u_username";
-	END IF; IF NEW."username" <> OLD."username"
-		AND OLD."username" IS NOT NULL THEN
-	RAISE WARNING 'Reverting change of username from % to %', OLD."username", NEW."username"; NEW."username" = OLD."username";
-	END IF; IF NEW."virgin" <> OLD."virgin"
-		AND OLD."virgin" = 0 THEN
-	RAISE WARNING 'Reverting change of virgin from % to %', OLD."virgin", NEW."virgin"; NEW."virgin" = OLD."virgin";
-	END IF; IF NEW."publicKey" <> OLD."publicKey"
-		AND OLD."virgin" = 0
-		AND OLD."publicKey" IS NOT NULL THEN
-	RAISE WARNING 'Reverting change of publicKey from % to %', ENCODE(OLD."publicKey", 'hex'), ENCODE(NEW."publicKey", 'hex'); NEW."publicKey" = OLD."publicKey";
-	END IF; IF NEW."secondPublicKey" <> OLD."secondPublicKey"
-		AND OLD."secondPublicKey" IS NOT NULL THEN
-	RAISE WARNING 'Reverting change of secondPublicKey from % to %', ENCODE(OLD."secondPublicKey", 'hex'), ENCODE(NEW."secondPublicKey", 'hex'); NEW."secondPublicKey" = OLD."secondPublicKey";
-	END IF; RETURN NEW;
+CREATE OR REPLACE FUNCTION public.revert_mem_account() RETURNS TRIGGER LANGUAGE PLPGSQL AS $function$
+	BEGIN
+		IF NEW."address" <> OLD."address" THEN
+			RAISE WARNING 'Reverting change of address from % to %', OLD."address", NEW."address"; NEW."address" = OLD."address";
+		END IF;
+		IF NEW."u_username" <> OLD."u_username" AND OLD."u_username" IS NOT NULL THEN
+			RAISE WARNING 'Reverting change of u_username from % to %', OLD."u_username", NEW."u_username"; NEW."u_username" = OLD."u_username";
+		END IF;
+		IF NEW."username" <> OLD."username" AND OLD."username" IS NOT NULL THEN
+			RAISE WARNING 'Reverting change of username from % to %', OLD."username", NEW."username"; NEW."username" = OLD."username";
+		END IF;
+		IF NEW."virgin" <> OLD."virgin" AND OLD."virgin" = 0 THEN
+			RAISE WARNING 'Reverting change of virgin from % to %', OLD."virgin", NEW."virgin"; NEW."virgin" = OLD."virgin";
+		END IF;
+		IF NEW."publicKey" <> OLD."publicKey" AND OLD."virgin" = 0 AND OLD."publicKey" IS NOT NULL THEN
+			RAISE WARNING 'Reverting change of publicKey from % to %', ENCODE(OLD."publicKey", 'hex'), ENCODE(NEW."publicKey", 'hex'); NEW."publicKey" = OLD."publicKey";
+		END IF;
+		IF NEW."secondPublicKey" <> OLD."secondPublicKey" AND OLD."secondPublicKey" IS NOT NULL THEN
+			RAISE WARNING 'Reverting change of secondPublicKey from % to %', ENCODE(OLD."secondPublicKey", 'hex'), ENCODE(NEW."secondPublicKey", 'hex'); NEW."secondPublicKey" = OLD."secondPublicKey";
+		END IF;
+	RETURN NEW;
 END $function$ ;
 
 END;
